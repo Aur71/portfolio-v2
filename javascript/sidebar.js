0cee0587afec6f306b2596a80a60b0abf762e09d
Aur71 <111057774+Aur71@users.noreply.github.com>
2023-05-11 19:33:15 +0300

    scrollbar done, sidebar done, navbar done, side icons done, side email done

--- javascript/sidebar.js
+++ javascript/sidebar.js
@@ -1,6 +1,24 @@
+const body = document.querySelector('body');
+const pageOverlay = document.getElementById('page-overlay');
 const sidebar = document.getElementById('sidebar');
 const openSidebarBtn = document.getElementById('open-sidebar-btn');
+const closeSidebarBtn = document.getElementById('close-sidebar-btn');
 
 openSidebarBtn.addEventListener('click', () => {
+  body.style.overflowY = 'hidden';
+  pageOverlay.classList.add('active');
   sidebar.classList.add('active');
+});
+
+closeSidebarBtn.addEventListener('click', () => {
+  body.style.overflowY = 'auto';
+  pageOverlay.classList.remove('active');
+  sidebar.classList.remove('active');
+});
+
+// closes the sidebar when you click a link
+window.addEventListener('scroll', () => {
+  body.style.overflowY = 'auto';
+  pageOverlay.classList.remove('active');
+  sidebar.classList.remove('active');
 });
